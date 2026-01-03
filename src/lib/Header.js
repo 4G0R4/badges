dd099e377cf3633fe452bc1b6871347c5d2e6bf3
ɅGOᏒɅ <agora_@tuta.io>
2026-01-03 18:28:33 +0100

    Change heading text from 'Badges' to 'SN Badges'

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
       height="80px"
     >
       <Link to="/">
-        <Heading as="h1">Badges</Heading>
+        <Heading as="h1">SN Badges</Heading>
       </Link>
       <Login />
     </Flex>
